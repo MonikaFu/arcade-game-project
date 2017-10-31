b4e280ae8d7bbacf052e0248e3873cde683f0501
MonikaFu <monika.furdyna@gmail.com>
2017-10-31 13:40:19 +0100

    Cosmetic changes to the code

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ var Enemy = function(y, speed) {
     // a helper that is provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     //initial horizontal position of the enemy
-    this.x = -201; 
+    this.x = -201;
     //initial vertical position of the enemy specified at instantiation
     // can be first, secnd or third row counting from the top
-    switch(y) { 
+    switch(y) {
     	case 1:
     		this.y = 62;
     		break;
@@ -19,18 +19,18 @@ var Enemy = function(y, speed) {
     	case 3:
     		this.y = 228;
     		break;
-    };
+    }
     //speed of the enemy is specified at instantiation
-    this.speed = speed; 
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // update the position of the enemy, relative to the time that passed between the 
+    // update the position of the enemy, relative to the time that passed between the
     // canvas update and enemy's speed
     this.x+=this.speed*dt;
-    // make sure that the enemy's movement is looped 
+    // make sure that the enemy's movement is looped
     if (this.x>606) {
     	this.x=-101;
     }
@@ -46,14 +46,14 @@ var Player = function() {
 	// Variables applied to each of our instances go here
 
 	// The image/sprite for our enemies, this uses
-    	// a helper that is provided to easily load images 
+    	// a helper that is provided to easily load images
 	this.sprite = 'images/char-boy.png';
 	// initial horizontal position of the player
 	this.x = 202;
 	// initial vertical position of the player
 	this.y = 380;
 	// initial state of the player
-	this.move = 'no move'
+	this.move = 'no move';
 };
 
 // update method of the player checks if it's state changed and if it's position
@@ -65,28 +65,28 @@ Player.prototype.update = function() {
 		case 'left':
 			if (this.x>0) {
 				this.x+=(-101);
-			};
-			this.move='no move'
+			}
+			this.move='no move';
 			break;
 		case 'right':
 			if (this.x<404) {
 				this.x+=101;
-			};
-			this.move='no move'
+			}
+			this.move='no move';
 			break;
 		case 'up':
 			if (this.y>-45) {
 				this.y+=(-85);
-			};
-			this.move='no move'
+			}
+			this.move='no move';
 			break;
 		case 'down':
 			if (this.y<380) {
 				this.y+=85;
-			};
-			this.move='no move'
+			}
+			this.move='no move';
 			break;
-	};
+	}
 };
 
 // Draw the player on the screen, required method for game
@@ -94,15 +94,15 @@ Player.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// handleInput method for the player. Changes the state of the player depending on a 
+// handleInput method for the player. Changes the state of the player depending on a
 // key that was pressed by the user
 Player.prototype.handleInput = function(movement) {
 	if (typeof(movement)!='undefined') {
 		this.move=movement;
 	} else {
-		this.move='no move'
-	};
-}; 
+		this.move='no move';
+	}
+};
 
 
 // Instantiation of the objects.
@@ -117,7 +117,7 @@ let allEnemies = [Enemy1, Enemy2, Enemy3, Enemy4, Enemy5];
 let player = new Player();
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. 
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -129,7 +129,7 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// Event listeners for the winning modal 
+// Event listeners for the winning modal
 
 // When the user clicks on <span> (x), close the modal
 $('.close').click(function() {
